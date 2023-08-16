9901f09b882e3169253ba833a58e6205f4da13ac
Aleksandra Klimantova <alexandra.klimantowa@gmail.com>
2023-08-16 18:40:36 +0200

    added methods on account

---
+++ put_v1_account_token.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def put_v1_account_token():
+    """
+    Activate registered user
+    :return:
+    """
+    token = '1234567890'
+    url = f"http://5.63.153.31:5051/v1/account/{token}"
+
+    headers = {
+        'X-Dm-Auth-Token': '',
+        'X-Dm-Bb-Render-Mode': '',
+        'Accept': 'text/plain'
+    }
+
+    response = requests.request(
+        method="PUT",
+        url=url,
+        headers=headers
+    )
+
+    return response
