6420546bb206ad714138def9be92e927b32d65b5
Aleksandra Klimantova <alexandra.klimantowa@gmail.com>
2023-09-11 23:15:13 +0200

    added possibility to work with DB using ORM

---
+++ generic/helpers/orm_db.py
@@ -0,0 +1,39 @@
+from typing import List
+from sqlalchemy import select, delete, update
+from generic.helpers.orm_models import User
+from orm_client.orm_client import OrmClient
+
+
+class OrmDatabase:
+    def __init__(self, user, password, host, database):
+        self.db = OrmClient(user, password, host, database)
+
+    def get_all_users(self):
+        query = select(User)
+        dataset = self.db.send_query(query)
+        return dataset
+
+    def get_user_by_login(self, login: str) -> List[User]:
+        query = (
+            select(User)
+            .where(User.Login == login)
+        )
+        dataset = self.db.send_query(query)
+        return dataset
+
+    def delete_user_by_login(self, login: str):
+        query = (
+            delete(User)
+            .where(User.Login == login)
+        )
+        dataset = self.db.send_bulk_query(query)
+        return dataset
+
+    def set_activated_flag_by_login(self, login: str, value: bool = True):
+        query = (
+            update(User)
+            .where(User.Login == login)
+            .values(Activated=value)
+        )
+        dataset = self.db.send_bulk_query(query)
+        return dataset
--- tests/test_post_v1_account.py
+++ tests/test_post_v1_account.py
@@ -1,6 +1,6 @@
 import time
 from generic.helpers.data_generator import DataGeneratorHelper
-from generic.helpers.dm_db import DmDatabase
+from generic.helpers.orm_db import OrmDatabase
 from services.dm_api_account import Facade
 
 
@@ -10,7 +10,7 @@ def test_post_v1_account():
     # Initialize data helper, API client and database client
     data_helper = DataGeneratorHelper()
     api = Facade(host="http://5.63.153.31:5051")
-    db = DmDatabase(user='postgres', password='admin', host='5.63.153.31', database='dm3.5')
+    orm = OrmDatabase(user='postgres', password='admin', host='5.63.153.31', database='dm3.5')
 
     # Register new user
     login = data_helper.generate_login()
@@ -23,17 +23,28 @@ def test_post_v1_account():
         password=password
     )
 
-    dataset = db.get_user_by_login(login=login)
+    dataset = orm.get_user_by_login(login=login)
     for row in dataset:
-        assert row['Login'] == login, f'User {login} is not registered'
-        assert row['Activated'] is False, f'User {login} is already activated'
+        assert row.Login == login, f'User {login} is not registered'
+        assert row.Activated is False, f'User {login} is already activated'
 
     # Activate registered user using DB
-    db.set_activated_flag_by_login(login=login)
-    activated_dataset = db.get_user_by_login(login=login)
-    time.sleep(2)
-    for row in activated_dataset:
-        assert row['Activated'] is True, f'User {login} is not activated'
+    orm.set_activated_flag_by_login(login=login)
+
+    def check_activation():
+        data = orm.get_user_by_login(login=login)
+        for r in data:
+            if r.Activated:
+                return True
+        return False
+
+    end_time = time.time() + 5  # 5 seconds timeout
+    while time.time() < end_time:
+        if check_activation():
+            break
+        time.sleep(1)
+    else:
+        assert False, f'User {login} was not activated in time'
 
     # Login as user
     api.login.login_user(
@@ -42,7 +53,7 @@ def test_post_v1_account():
     )
 
     # Delete user, delete emails - cleanup data
-    db.delete_user_by_login(login=login)
-    get_user_from_db = db.get_user_by_login(login=login)
+    orm.delete_user_by_login(login=login)
+    get_user_from_db = orm.get_user_by_login(login=login)
     assert len(get_user_from_db) == 0, f'User with {login} is not deleted'
     api.mailhog.delete_all_messages()
